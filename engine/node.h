5c9694aae522cf17a1cb92bd60095338248cb8b8
MarcoliniAndrea0 <andrea.marcolini@student.supsi.ch>
2025-11-19 15:55:12 +0100

    Fix error on getlocalMatrix() and getWorldMatrix()

--- engine/node.h
+++ engine/node.h
@@ -66,15 +66,15 @@ public:
     
     /**
      * @brief Restituisce la matrice di trasformazione locale.
-     * @return Puntatore alla matrice locale.
+     * @return La matrice locale.
      */
-    glm::mat4* getLocalMatrix();
+    glm::mat4 getLocalMatrix();
     
     /**
      * @brief Restituisce la matrice di trasformazione globale.
-     * @return Puntatore alla matrice globale.
+     * @return La matrice globale.
      */
-    glm::mat4* getWorldMatrix();
+    glm::mat4 getWorldMatrix();
     
     /**
      * @brief Ricerca ricorsivamente un nodo per nome in tutta la sotto-gerarchia.
