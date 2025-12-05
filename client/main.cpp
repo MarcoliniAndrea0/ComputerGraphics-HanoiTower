d8062d79ebe89fa853ca1c1269a63bc10cad04d4
MarcoliniAndrea0 <andrea.marcolini@student.supsi.ch>
2025-12-05 13:05:53 +0100

    clear main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -10,14 +10,10 @@
 //////////////
 
 // Library header:
-#include "engine.h"
-#include "definitions.h"
+
 
 //#include "texture.h"
-#include "material.h"
-#include "light.h"
-#include "directionalLight.h"
-#include "prospectiveCamera.h"
+
 
 // C/C++:
 #include <iostream>
@@ -27,54 +23,8 @@
 //////////
 
 // callbacks
-void keyboardCallback(unsigned char key, int mouseX, int mouseY)
-{
 
-   // example, actual behaviour yet to be defined
-   switch (key)
-   {
-   case ' ':;
-      break;
-   case 'a':;
-      break;
-   case 'b':;
-      break;
-   case 'c':;
-      break;
-   case 'r':
-      //   colorR = rand() % 256;
-      //   colorG = rand() % 256;
-      //   colorB = rand() % 256;
-      break;
-   default:;
-      break;
-   }
-}
 
-void specialCallback(unsigned char key, int mouseX, int mouseY)
-{
-
-   std::cout << "[key pressed]" << std::endl;
-
-   switch (key)
-   {
-   case ENG_KEY_UP:
-      ;
-      break;
-
-   case ENG_KEY_DOWN:
-      ;
-      break;
-
-   case ENG_KEY_LEFT:
-      ;
-      break;
-
-   case ENG_KEY_RIGHT:
-      ;
-      break;
-   }
-}
 
 /**
  * Application entry point.
@@ -89,23 +39,12 @@ int main(int argc, char *argv[])
    std::cout << std::endl;
 
    // Init engine:
-   Eng::Base &eng = Eng::Base::getInstance();
-   eng.init("My application", 500, 500, argc, argv);
 
-   //Texture* myTex = new Texture("TestTex", "test.jpg"); // Sostituisci col nome del tuo file
-   Material* myMat = new Material("TestMat");
-   //myMat->setTexture(myTex);
-   myMat->setDiffuse(glm::vec4(1.0f)); // Bianco per far risaltare la texture
-
-   eng.setKeyboardCallback(keyboardCallback);
 
    // test components
-   eng.testGLM();
-   eng.drawSolidSphere(10,10,10);
-   // eng.testObjectGeneration();
+   
 
    // Release engine:
-   eng.free();
 
    // Done:
    std::cout << "\n[application terminated]" << std::endl;
